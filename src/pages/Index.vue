c051ff36a4a0bdf151efc498c01cbd76f91db77a
ldiebold <luke@ldiebold.com>
2020-10-04 15:21:22 +1100

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn
+      color="red"
+      push
+      icon="flag"
+      round
+      size="xl"
+      @click="$q.notify({
+        message: 'Crushing it at quasar!',
+        color: 'blue',
+        position: 'center',
+        icon: 'thumb_up'
+      })"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
