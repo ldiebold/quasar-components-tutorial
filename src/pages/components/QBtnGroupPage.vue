a0e2c5082c0e263cf54e6f757218a99765a9598d
ldiebold <luke@ldiebold.com>
2020-10-11 18:08:55 +1100

    q-btn-group

---
+++ src/pages/components/QBtnGroupPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page padding>
+    <q-btn-group
+      push
+      spread
+    >
+      <q-btn
+        icon="home"
+        push
+        label="one"
+      />
+      <q-btn
+        icon="person"
+        push
+        label="two"
+      />
+      <q-btn-dropdown
+        push
+        label="three"
+        split
+      >
+        <div class="q-pa-md">
+          The Menu
+        </div>
+      </q-btn-dropdown>
+    </q-btn-group>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
