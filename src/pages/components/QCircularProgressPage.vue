6226fafb7bb55b076e59a3d5a40b16ee61234e02
ldiebold <luke@ldiebold.com>
2020-10-18 13:46:12 +1100

    q-circular-progress

---
+++ src/pages/components/QCircularProgressPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page
+    padding
+    class="q-pa-xl"
+  >
+    <q-circular-progress
+      show-value
+      :value="70"
+      font-size="16px"
+      size="180px"
+      color="indigo"
+      :thickness="0.3"
+      track-color="indigo-2"
+      center-color="indigo-1"
+    >
+      <div class="q-pa-xl text-center">
+        course completion
+      </div>
+    </q-circular-progress>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
