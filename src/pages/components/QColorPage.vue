ce2c98f50b839821e6284813f7f71d9a6f962d98
ldiebold <luke@ldiebold.com>
2020-10-18 16:52:48 +1100

    q-color

---
+++ src/pages/components/QColorPage.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page padding>
+    <q-input
+      v-model="hex"
+      :rules="[
+        val => testIsColor(val) || 'please enter a valid date'
+      ]"
+    >
+      <template #prepend>
+        <q-avatar :style="`background-color: ${hex}`" />
+      </template>
+
+      <template #append>
+        <q-icon
+          class="cursor-pointer"
+          name="colorize"
+        >
+          <q-popup-proxy>
+            <q-color v-model="hex" />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+  </q-page>
+</template>
+
+<script>
+import { patterns } from 'quasar'
+
+export default {
+  methods: {
+    handleSubmit (event) {
+      const formData = new FormData(event.target)
+
+      for (const [key, value] of formData.entries()) {
+        console.log(key)
+        console.log(value)
+      }
+    }
+  },
+
+  data () {
+    return {
+      hex: '#84ff80',
+      hexa: '#94a4ff80',
+      rgb: 'rgb(255, 100, 100)',
+      rgba: 'rgba(255, 40, 40, 0.5)',
+      testIsColor: patterns.testPattern.anyColor
+    }
+  }
+}
+</script>
